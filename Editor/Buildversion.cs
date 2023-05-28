fbbe4a5dd889637a9545c01bcf416505c4e7c802
Alex Ennerfelt <alex.ennerfelt@gmail.com>
2023-05-28 16:31:45 +0800

    feature: added build target to be included in the version field

--- Editor/Buildversion.cs
+++ Editor/Buildversion.cs
@@ -14,7 +14,7 @@ namespace Ennerfelt.GitVersioning
     public class BuildVersion : IPreprocessBuildWithReport, IPostprocessBuildWithReport
     {
         public int callbackOrder => 0;
-
+        private string _previousBuildVersion;
 
         public static bool AreVersionsMatching() {
             var v = PlayerSettings.bundleVersion;
@@ -24,32 +24,62 @@ namespace Ennerfelt.GitVersioning
         }
         public static int? GetBuildNumber() {
             var v = PlayerSettings.bundleVersion;
-            var b = v.Substring(v.LastIndexOf("b") + 1);
             try {
+                var b = v.Substring(v.LastIndexOf("b") + 1);
                 int result = Int32.Parse(b);
                 return result;
-            } catch (Exception) {
+            }
+            catch (Exception) {
                 return null;
             }
 
         }
 
         public void OnPostprocessBuild(BuildReport report) {
-            Debug.Log($"{PlayerSettings.bundleVersion} Sucessfully built");
-            if (report.summary.result == BuildResult.Succeeded) {
-            }
+            Debug.Log(PlayerSettings.bundleVersion);
+            Debug.Log("Successful build");
         }
 
         public void OnPreprocessBuild(BuildReport report) {
             var build = GetBuildNumber();
-            if (AreVersionsMatching()) {
-                build++;
-            } else {
-                build = 0;
+            if (build.HasValue) {
+                if (AreVersionsMatching()) {
+                    build = 0;
+                }
+                else {
+                    build++;
+                }
+                PlayerSettings.bundleVersion = $"{Git.BuildVersion} {GetOS()}-b{build}";
             }
-
-            // This gets the Build Version from Git via the `git describe` command
-            PlayerSettings.bundleVersion = Git.BuildVersion + "-b" + build;
+            else {
+                PlayerSettings.bundleVersion = $"{Git.BuildVersion} {GetOS()}";
+            }
         }
-    } 
+        private string GetOS() {
+            var os = EditorUserBuildSettings.activeBuildTarget;
+
+            switch (os) {
+                case BuildTarget.StandaloneOSX:
+                    return "OSX";
+                case BuildTarget.StandaloneWindows:
+                    return "Win-32";
+                case BuildTarget.StandaloneWindows64:
+                    return "Win-64";
+                case BuildTarget.WSAPlayer:
+                    return "Wsa";
+                case BuildTarget.StandaloneLinux64:
+                    return "Linux";
+                case BuildTarget.PS4:
+                    return "PS4";
+                case BuildTarget.XboxOne:
+                    return "XboxOne";
+                case BuildTarget.Switch:
+                    return "Switch";
+                case BuildTarget.GameCoreXboxOne:
+                    return "GCXboxOne";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
 }
